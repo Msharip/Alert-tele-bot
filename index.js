691b0fc18a93caee36fc2509ae6656491dd2c404
Msharip <mshari5401@gmail.com>
2024-09-16 11:49:19 +0300

    removing tele.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
 
-// نحتاج لتفعيل Webhook في tele.js فقط
-app.use(express.json());
 
 // SUB-CHANNELS
 require('./channel-alert/channelalert.js'); // هذا الملف يستخدم Polling
 
-// تضمين tele.js الذي يستخدم Webhook
-require('./tele/tele.js')(app); // تمرير app حتى يتمكن من استخدام Webhook
-
-console.log("Bots are Running");
 
 // بدء الخادم لاستقبال طلبات Webhook
 const PORT = process.env.PORT || 3000;
