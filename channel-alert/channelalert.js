e927d655601cb3045db9a633c8a131838759d469
Msharip <mshari5401@gmail.com>
2024-09-22 13:36:56 +0300

    changing time check v2

--- channel-alert/channelalert.js
+++ channel-alert/channelalert.js
@@ -218,7 +218,7 @@ async function checkHomePage() {
 function checkAllRandomly() {
   console.log('Checking product')
   checkHomePage();
-  const randomInterval = Math.floor(Math.random() * (9000 - 5000 + 1)) + 5000;
+  const randomInterval = Math.floor(Math.random() * (7000 - 5000 + 1)) + 3000;
   setTimeout(checkAllRandomly, randomInterval);
 }
 
