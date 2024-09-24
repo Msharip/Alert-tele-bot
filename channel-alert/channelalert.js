4a172575b6afcfadcce58a705e20eeef93aade98
Msharip <mshari5401@gmail.com>
2024-09-24 16:54:18 +0300

    change time

--- channel-alert/channelalert.js
+++ channel-alert/channelalert.js
@@ -125,7 +125,7 @@ async function checkHomePage() {
           const inventoryQuantity = await getInventoryDetails(`https://www.dzrt.com/ar-sa/products/${productUrl}`);
 
           // تحقق إذا كانت الكمية أكبر من 0
-          if (inventoryQuantity > 4) {
+          if (inventoryQuantity > 3) {
             const imageUrlAvailable = path.join(__dirname, '..', 'images', `${productNames[productUrl].en}.png`);
 
             const messageAvailable = `
@@ -235,7 +235,8 @@ async function checkHomePage() {
 
 function checkAllRandomly() {
   checkHomePage();
-  const randomInterval = Math.floor(Math.random() * (15000 - 10000 + 1)) + 10000;
+ // const randomInterval = Math.floor(Math.random() * (15000 - 10000 + 1)) + 10000;
+ const randomInterval = Math.floor(Math.random() * (4000 - 3000 + 1)) + 3000;
   setTimeout(checkAllRandomly, randomInterval);
 }
 
